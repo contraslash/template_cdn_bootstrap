4e626902f9418586462d6ebc2356dd77a7e16eb5
Mauricio Collazos <ma0@contraslash.com>
2019-04-07 13:31:35 -0500

    Added context processor, updated readme and added license.
    To use sidebar with menus we add a new context processor to manage dynamic menus.
    
    Base html was also modified and intructions were detailed on README

--- README.md
+++ README.md
@@ -4,3 +4,23 @@ This is a simple  Django template that uses bootstrap from a CND
 
 Sidebar was created based on [Pro Sidebar](https://bootsnipp.com/snippets/Q0dAX)
 by [Bootsnip](https://bootsnipp.com/)
+
+To use sidebar use the next schema in your `conf.py`
+
+```
+MENUS = {
+    "name": _("<menu_dropdown_title>"),
+    "fa_icon": "fa-<some_awesome_font>",
+    "sub_menus": [
+        {
+            "name": _("<submenu_name>"),
+            "url_reversed": reverse_lazy(<your_url_name>),
+            "permissions": ["<permission_to_access_the_view>"]
+
+        }
+    ]
+}
+```
+
+Also add `applications.base_template.context_processors.menus` to your
+context processors.
---
+++ context_processors.py
@@ -0,0 +1,38 @@
+import importlib
+from operator import and_
+from functools import reduce
+
+from django.conf import LazySettings
+
+settings = LazySettings()
+
+
+def menus(request):
+    menus = list()
+    for app in settings.INSTALLED_APPS:
+        app_label = app.split(".")[-1]
+        app_menu = get_menu_from_conf_file(app)
+        if app_menu is not None:
+            visible_menu_for_user = app_menu.copy()
+            visibles_submenus = list()
+            for submenu in app_menu.get("sub_menus", list()):
+                permissions = submenu.get("permissions", list())
+                if (
+                        permissions and reduce(and_, (request.user.has_perm(f"{app_label}.{x}") for x in permissions))
+                ) or request.user.is_staff:
+                    visibles_submenus.append(submenu)
+            if visibles_submenus:
+                visible_menu_for_user["sub_menus"] = visibles_submenus
+                menus.append(visible_menu_for_user)
+    print(menus)
+    return {
+        "menus": menus
+    }
+
+
+def get_menu_from_conf_file(module_name):
+    try:
+        conf_module = importlib.import_module(f"{module_name}.conf")
+        return conf_module.MENUS
+    except (ModuleNotFoundError, AttributeError) as e:
+        return
